455cecaec3454d55851557ac945d1819b131b407
Daniel Lindsley <daniel@toastdriven.com>
2023-10-13 14:16:15 -0500

    Added Avatar.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
+import 'preact/debug';
 import { render } from 'preact';
 import { useState } from 'preact/hooks';
 
 import './style.css';
 
+import Avatar from './components/Avatar';
 import Button from './components/Button';
 import ErrorBoundary from './components/ErrorBoundary';
 import Errors from './components/Errors';
@@ -56,7 +58,7 @@ export function App() {
   }
 
   return (
-    <div className="mx-auto my-8 w-3/4">
+    <div className="mx-auto my-8 p-8 w-3/4 bg-gray-200">
       <Header
         label="My Component Library"
       />
@@ -93,6 +95,48 @@ export function App() {
             onChange={changePassword}
           />
         </InlineFormRow>
+
+        <div className="underline">
+          <Header
+            label="Avatars"
+            size="md"
+          />
+        </div>
+
+        <div className="my-4 flex flex-row space-x-1">
+          <Avatar
+            name="DRL"
+          />
+          <Avatar
+            name="AR"
+          />
+          <Avatar
+            name="DL"
+            size={12}
+          />
+          <Avatar
+            name="MB"
+            color="amber-800"
+            size={32}
+          />
+          <Avatar
+            name={username}
+            size={20}
+            roundingAmount="lg"
+          />
+          <Avatar
+            name={username}
+            size={12}
+            imageUrl="https://avatars.githubusercontent.com/u/2449?v=4"
+          />
+        </div>
+
+        <div className="underline">
+          <Header
+            label="Loading"
+            size="md"
+          />
+        </div>
 
         <div className="text-black mx-auto my-4 w-5">
           <Loading />
@@ -144,6 +188,22 @@ export function App() {
           />
         </div>
       </Form>
+
+      {/*
+        Because Tailwind's JIT is being dumb about dynamic class names, this
+        ensures all the classes for the <Avatar /> component are present.
+      */}
+      <div className="hidden">
+        <div className="bg-red-500 h-8 w-8 rounded-none text-white font-bold flex items-center justify-center"></div>
+        <div className="bg-pink-500 h-4 w-4 rounded-md"></div>
+        <div className="bg-orange-500 h-12 w-12 rounded-lg"></div>
+        <div className="bg-yellow-500 h-32 w-32 rounded-full"></div>
+        <div className="bg-green-500 h-20 w-20 text-xs"></div>
+        <div className="bg-blue-500 text-sm"></div>
+        <div className="bg-purple-500 text-md"></div>
+        <div className="bg-slate-400 text-lg"></div>
+        <div className="bg-amber-800 text-2xl"></div>
+      </div>
     </div>
   );
 }
